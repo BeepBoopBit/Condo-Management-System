394bf4680c3de367870065c2a8f1813d1aa43084
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-07-13 23:19:51 +0800

    Added Datas.dat

--- src/FileManager/istream.java
+++ src/FileManager/istream.java
@@ -3,13 +3,25 @@ package FileManager;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class istream {
-    private ArrayList<String> _data;
-    
+    ArrayList<String> _names;
+    ArrayList<String> _ages;
+    ArrayList<String> _locations;
+    ArrayList<String> _paymentOptions;
+    ArrayList<ArrayList<String>> _details;
+    ArrayList<ArrayList<String>> _amenities;
+
     public istream(){
-        _data = new ArrayList<>();
+        _names = new ArrayList<>();
+        _ages = new ArrayList<>();
+        _locations = new ArrayList<>();
+        _paymentOptions = new ArrayList<>();
+        _details = new ArrayList<>();
+        _amenities = new ArrayList<>();
     }
     
     public void read(String path){
@@ -17,14 +29,44 @@ public class istream {
             File myFile = new File(path);
             Scanner myReader = new Scanner(myFile);
             while(myReader.hasNextLine()){
-                _data.add(myReader.nextLine());
+                _names.add(myReader.nextLine());
+                _ages.add(myReader.nextLine());
+                _locations.add(myReader.nextLine());
+                _paymentOptions.add(myReader.nextLine());
+                ArrayList<String> tempDetails = new ArrayList<>();
+                Collections.addAll(tempDetails, myReader.nextLine().split(" "));
+                _details.add(tempDetails);
+                ArrayList<String> tempAmenities = new ArrayList<>();        
+                Collections.addAll(tempAmenities, myReader.nextLine().split(" "));
+                _amenities.add(tempAmenities);
             }
         }catch(FileNotFoundException err){
             System.out.println("The file was not found");
         }
     }
     
-    public ArrayList<String> getData(){
-        return _data;
+    public void print(){
+        for(int i = 0; i < _names.size(); ++i){
+            System.out.println(_names.get(i));
+        }
+        for(int i = 0; i < _ages.size(); ++i){
+            System.out.println(_ages.get(i));
+        }
+        for(int i = 0; i < _locations.size(); ++i){
+            System.out.println(_locations.get(i));
+        }
+        for(int i = 0; i < _paymentOptions.size(); ++i){
+            System.out.println(_paymentOptions.get(i));
+        }
+        for(int i = 0; i < _details.size(); ++i){
+            for(int j = 0; j < _details.get(i).size(); ++i){
+                System.out.println(_details.get(i).get(j));
+            }
+        }
+        for(int i = 0; i < _amenities.size(); ++i){
+            for(int j = 0; j < _amenities.get(i).size(); ++i){
+                System.out.println(_amenities.get(i).get(j));
+            }
+        }
     }
 }
